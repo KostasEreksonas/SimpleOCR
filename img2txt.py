9eff87238b5306ff04f1628f451c8d9ded3ec385
KostasEreksonas <k.ereksonas@gmail.com>
2023-10-30 16:44:21 +0200

    Extract text from noisy image

--- img2txt.py
+++ img2txt.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
 
 from PIL import Image
+import cv2
 import pytesseract
 import numpy as np
 
+#  ---------
+# | Image 1 |
+#  ---------
+
 filename1 = "sample1.jpg"
-filename2 = "sample2.jpg"
 
 img1 = np.array(Image.open(filename1))
-img2 = np.array(Image.open(filename2))
-text1 = pytesseract.image_to_string(img1)
-text2 = pytesseract.image_to_string(img2)
 
-print(text1)
-print(text2)
+text1 = pytesseract.image_to_string(img1)
+print(f"Result: {text1}")
+
+#  ---------
+# | Image 2 |
+#  ---------
+
+filename2 = "sample2.jpg"
+
+img2 = np.array(Image.open(filename2))
+cv2norm_img = np.zeros((img2.shape[0], img2.shape[1]))
+img2 = cv2.normalize(img2, cv2norm_img, 0, 255, cv2.NORM_MINMAX)
+img2 = cv2.threshold(img2, 100, 255, cv2.THRESH_BINARY)[1]
+img2 = cv2.GaussianBlur(img2, (1, 1), 0)
+
+text2 = pytesseract.image_to_string(img2)
+print(f"Result: {text2}")
