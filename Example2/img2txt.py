6172dcfe5fd773ef659652ca9e062231ddbc1055
KostasEreksonas <k.ereksonas@gmail.com>
2023-10-31 13:38:16 +0200

    Initial code

---
+++ Example2/img2txt.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+image = cv2.imread('image.jpg')
+
+# plot image
+def plot_image(image):
+    b, g, r = cv2.split(image)
+    rgb_img = cv2.merge([r,g,b])
+    plt.imshow(rgb_img)
+    plt.title('AUREBESH ORIGINAL IMAGE')
+    plt.show()
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+
+#thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#dilation
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+
+#erosion
+def erode(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.erode(image, kernel, iterations = 1)
+
+#opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+#canny edge detection
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+#skew correction
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
+#template matching
+def match_template(image, template):
+    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+
+gray = get_grayscale(image)
+thresh = thresholding(gray)
+opening = opening(gray)
+canny = canny(gray)
+
+plot_image(image)
+print(f"Gray:\n{gray}\nThreshold:\n{thresh}\nOpening:\n{opening}\nCanny:\n{canny}")
